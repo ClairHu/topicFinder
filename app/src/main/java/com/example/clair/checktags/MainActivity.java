faa5610a3ba04f98525b9cc112625d74a8a5c5f2
ClairHu <z74hu@edu.uwaterloo.ca>
2018-05-06 08:56:41 -0400

    add files

---
+++ app/src/main/java/com/example/clair/checktags/MainActivity.java
@@ -0,0 +1,170 @@
+package com.example.clair.checktags;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class Item {
+        boolean checked;
+        String ItemString;
+        Item(String t, boolean b){
+            ItemString = t;
+            checked = b;
+        }
+
+        public boolean isChecked(){
+            return checked;
+        }
+    }
+
+    static class ViewHolder {
+        CheckBox checkBox;
+        TextView text;
+    }
+
+    public class ItemsListAdapter extends BaseAdapter {
+        private Context context;
+        private List<Item> list;
+
+        ItemsListAdapter(Context c, List<Item> l){
+            context = c;
+            list = l;
+        }
+
+        @Override
+        public Object getItem(int position){
+            return list.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public boolean isChecked(int position) {
+            return list.get(position).checked;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            View rowView = convertView;
+
+            ViewHolder viewHolder = new ViewHolder();
+            if (rowView == null) {
+                LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+                rowView = inflater.inflate(R.layout.row, null);
+
+                viewHolder.checkBox = (CheckBox) rowView.findViewById(R.id.rowCheckBox);
+                viewHolder.text = (TextView) rowView.findViewById(R.id.rowTextView);
+                rowView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) rowView.getTag();
+            }
+
+            viewHolder.checkBox.setChecked(list.get(position).checked);
+
+            final String itemStr = list.get(position).ItemString;
+            viewHolder.text.setText(itemStr);
+
+            viewHolder.checkBox.setTag(position);
+
+            viewHolder.checkBox.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view){
+                    boolean newState = !list.get(position).isChecked();
+                    list.get(position).checked = newState;
+                    Toast.makeText(getApplicationContext(),
+                            itemStr + "setOnClickListener\nchecked: " + newState,
+                            Toast.LENGTH_LONG).show();
+                }
+            } );
+
+            viewHolder.checkBox.setChecked(isChecked(position));
+
+            return rowView;
+        }
+    }
+
+    Button btnRegister;
+    List<Item> items;
+    ListView listview;
+    ItemsListAdapter myItemsListAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listview = (ListView)findViewById(R.id.listview);
+        btnRegister = (Button)findViewById(R.id.register);
+
+        initItems();
+        myItemsListAdapter = new ItemsListAdapter(this, items);
+        listview.setAdapter(myItemsListAdapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                Toast.makeText(MainActivity.this, ((Item)(parent.getItemAtPosition(position))).ItemString, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String str = "Check items:\n";
+
+                for (int i=0; i<items.size(); i++){
+                    if (items.get(i).isChecked()){
+                        str += i + "\n";
+                    }
+                }
+
+                Toast.makeText(MainActivity.this,
+                        str,
+                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    private void initItems() {
+        items = new ArrayList<Item>();
+
+        TypedArray arrayDrawable = getResources().obtainTypedArray(R.array.resicon);
+        TypedArray arrayText = getResources().obtainTypedArray(R.array.restext);
+
+        for (int i = 0; i < arrayDrawable.length(); i++) {
+            String s = arrayText.getString(i);
+            boolean b = false;
+            Item item = new Item(s, b);
+            items.add(item);
+        }
+
+        arrayDrawable.recycle();
+        arrayText.recycle();
+    }
+}
